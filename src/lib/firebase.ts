0d697c6248e1f0d955287d25ec5bbb729548e666
NousInfotech <noustest142@gmail.com>
2025-07-17 20:20:29 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -38,13 +38,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEU6pw22r_OUTEdjhfj1G8uISIuGE0VEA",
-  authDomain: "sheetsway-23ef6.firebaseapp.com",
-  projectId: "sheetsway-23ef6",
-  storageBucket: "sheetsway-23ef6.firebasestorage.app",
-  messagingSenderId: "167397766634",
-  appId: "1:167397766634:web:c03889ecfff951c9e1ae93",
-  measurementId: "G-41WDB478CP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 };
 
 export const app = initializeApp(firebaseConfig);
